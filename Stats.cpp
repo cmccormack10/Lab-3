e7b08a9cb371b6efdd1088edb8823293a41b384c
cmccormack10 <crm030314@gmail.com>
2023-10-12 10:08:42 -0500

    Lab 3 updated

--- Stats.cpp
+++ Stats.cpp
@@ -1,57 +1,71 @@
 #include "Lab3Stats.h"
 #include <cmath>
 
+// Constructor for stats class
 stats::stats() {
-	float num1 = 0;
-	float num2 = 0;
-	float num3 = 0;
-	float num4 = 0;
-	float statsmean = 0;
+    // Initialize variables to 0
+    float num1 = 0;
+    float num2 = 0;
+    float num3 = 0;
+    float num4 = 0;
+    float statsmean = 0;
 }
 
-stats::stats(float num1_, float num2_, float num3_, float num4_, float statsmean_) 
+// Parameter constructor for stats class
+stats::stats(float num1_, float num2_, float num3_, float num4_, float statsmean_)
 {
-	num1 = num1_;
-	num2 = num2_;
-	num3 = num3_;
-	num4 = num4_;
-	statsmean = statsmean_;
+    // Initialize variables
+    num1 = num1_;
+    num2 = num2_;
+    num3 = num3_;
+    num4 = num4_;
+    statsmean = statsmean_;
 }
 
+// Destructor for stats class
 stats::~stats() {}
 
+// Set function to set the value of num1
 void stats::setnum1(float num1_)
 {
-	num1 = num1_;
+    num1 = num1_;
 }
 
+// Setter function to set the value of 'num2'
 void stats::setnum2(float num2_)
 {
-	num2 = num2_;
+    num2 = num2_;
 }
 
+// Set function to set the value of num3
 void stats::setnum3(float num3_)
 {
-	num3 = num3_;
+    num3 = num3_;
 }
 
+// Set function to set the value of num4
 void stats::setnum4(float num4_)
 {
-	num4 = num4_;
+    num4 = num4_;
 }
 
-
+// Calculate and return the mean of num1, num2, num3, and num4
 float stats::mean()
 {
-	float statsmean = (num1 + num2 + num3 + num4)/4.0;
+    // Calculate mean and store it in statsmean
+    float statsmean = (num1 + num2 + num3 + num4) / 4.0;
 
-	return statsmean;
+    return statsmean;
 }
 
+// Calculate and return the population standard deviation of 'num1', 'num2', 'num3', and 'num4'
 float stats::populationStandardDeviation()
 {
-	float squared_deviations_sum = pow(num1 - mean(), 2) + pow(num2 - mean(), 2) + pow(num3 - mean(), 2) + pow(num4 - mean(), 2);
-	float populationStandardDev = sqrt(squared_deviations_sum/4.0);
-	return populationStandardDev;
+    // Calculate the squared deviations sum
+    float squared_deviations_sum = pow(num1 - mean(), 2) + pow(num2 - mean(), 2) + pow(num3 - mean(), 2) + pow(num4 - mean(), 2);
 
+    // Calculate the population standard deviation and store it in populationStandardDev
+    float populationStandardDev = sqrt(squared_deviations_sum / 4.0);
+
+    return populationStandardDev;
 }
--- main.cpp
+++ main.cpp
@@ -1,85 +1,89 @@
-#include "Lab3Stats.h"
-#include <iostream>
-#include <iomanip>
-#include <fstream>
+/*
+    Chris McCormack 
+    C++ Fall 2023 
+    Lab 3 - User and File I/O
+    Write an interactive C++ program that computes and outputs the mean and
+    population standard deviation of a set of four integers that are inputted by a file called
+    “inMeanStd.dat” and the user
+*/
 
-using namespace std;
+#include "Lab3Stats.h" 
+#include <iostream>     
+#include <iomanip>      
+#include <fstream>      
 
-int main()
-{
-	float num1, num2, num3, num4;
-	stats mystats;
+using namespace std;   
 
-	// Take user inputs and use setter functions to set the values
-	cout << "Enter the first number: ";
-	cin >> num1;
-	mystats.setnum1(num1);
+int main() {
+    float num1, num2, num3, num4;  // Declares the 4 numbers as floats
+    stats mystats;                // Creates object for stats class
 
-	cout << "Enter the second number: ";
-	cin >> num2;
-	mystats.setnum2(num2);
+    // Take user inputs and use set functions to set the values
+    cout << "Enter the first number: ";
+    cin >> num1;
+    mystats.setnum1(num1);
 
-	cout << "Enter the third number: ";
-	cin >> num3;
-	mystats.setnum3(num3);
+    cout << "Enter the second number: ";
+    cin >> num2;
+    mystats.setnum2(num2);
 
-	cout << "Enter the fourth number: ";
-	cin >> num4;
-	mystats.setnum4(num4);
+    cout << "Enter the third number: ";
+    cin >> num3;
+    mystats.setnum3(num3);
 
-	// Calculate mean and population standard deviation using member functions
-	float statsmean = mystats.mean();
-	float populationStandardDev = mystats.populationStandardDeviation();
+    cout << "Enter the fourth number: ";
+    cin >> num4;
+    mystats.setnum4(num4);
 
-	cout << "The mean of the 4 numbers is: " << statsmean << endl;
-	cout << "The population standard deviation of the 4 numbers is: " << setprecision(5) << populationStandardDev << endl;
-	
-	// opens meanstd file to take file input
-	ifstream inFile;
-	inFile.open("inMeanStd.dat");
+    // Calculation section
+    float statsmean = mystats.mean();                   // Calculate the mean of the four numbers
+    float populationStandardDev = mystats.populationStandardDeviation();  // Calculate the population standard deviation
 
-		// error message if can't find or open file
-		if (!inFile) {
-			
-			cout << "Error opening file";
-			return 0;
-		}
-	
+    cout << "The mean of the 4 numbers is: " << statsmean << endl;
+    cout << "The population standard deviation of the 4 numbers is: " << setprecision(5) << populationStandardDev << endl;
 
-	// assigning file data to override the number identifiers
-		inFile >> num1 >> num2 >> num3 >> num4;
+    // Open the "inMeanStd.dat" file to prep for assigning to new variables
+    ifstream inFile;
+    inFile.open("inMeanStd.dat");
 
-	stats filestats;
+    // Check if the file can't be opened
+    if (!inFile) {
+        cout << "Error opening file";
+        return 1;
+    }
 
-	filestats.setnum1(num1);
-	filestats.setnum2(num2);
-	filestats.setnum3(num3);
-	filestats.setnum4(num4);
+    // Read data from the file and overides the numbers
+    inFile >> num1 >> num2 >> num3 >> num4;
 
-	statsmean = filestats.mean();
+    stats filestats;  // Creates new object for the file data
 
-	int size = 4;
+    // Set the values from the file to the filestats object
+    filestats.setnum1(num1);
+    filestats.setnum2(num2);
+    filestats.setnum3(num3);
+    filestats.setnum4(num4);
 
-	populationStandardDev = filestats.populationStandardDeviation();
+    // Recalculate the mean and population standard deviation from file data
+    statsmean = filestats.mean();
+    populationStandardDev = filestats.populationStandardDeviation();
 
-	// Outputting data to outMeanstd.dat file
-	ofstream outFile;
-	outFile.open("outMeanstd.dat");
+    // Open the "outMeanstd.dat" file to prep for outputs of the calctulations
+    ofstream outFile;
+    outFile.open("outMeanstd.dat");
 
-		// error message if can't find or open file
-		if (!outFile)
-		{
-			cout << "Error opening file" << endl;
-			return 1;
-		}
+    // Check if the file couldn't be opened
+    if (!outFile) {
+        cout << "Error opening file" << endl;
+        return 1;
+    }
 
-	outFile << "The mean of the file numbers is: " << filestats.mean() << endl;
+    // outputs calculated statistics to the "outMeanstd.dat" file
+    outFile << "The mean of the file numbers is: " << filestats.mean() << endl;
+    outFile << "The population standard deviation of the file numbers is: " << setprecision(5) << populationStandardDev << endl;
 
-	outFile << "The population standard deviation of the file numbers is: " << setprecision(5) << populationStandardDev << endl;
+    // Close the I/O files
+    outFile.close();
+    inFile.close();
 
-	// Closing Files 
-	outFile.close();
-	inFile.close();
-
-	return 0;
+    return 0;
 }
